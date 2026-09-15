5bce14e37aa4284985388d126ccb26948048145e
germanando-ux <german.anton.dopazo@gmail.com>
2026-09-15 11:40:07 +0200

    Creación de CreateProductValidator

---
+++ BackendMaster2.Api/Validators/CreateProductValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using BackendMaster2.Api.Models;
+
+namespace BackendMaster2.Api.Validators;
+using BackendMaster2.Api.Models;
+public class CreateProductValidator : AbstractValidator<CreateProductRequest>
+{
+    public CreateProductValidator()
+    {
+
+    }
+}
